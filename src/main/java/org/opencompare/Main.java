5028860eb7f335cba985be4e4cee9fbf92a6f803
lewanni <quang532@gmail.com>
2016-11-07 22:19:49 +0100

    update generation html avec un chargement d'un nouveau pcm

--- src/main/java/org/opencompare/Main.java
+++ src/main/java/org/opencompare/Main.java
@@ -10,13 +10,15 @@ public class Main {
 	public static void main(String[] args) throws IOException, TemplateException {
 		
 		// Chargement fichier + pcm + traitement des types
-		File file = new File("pcms/example.pcm");
+		File file = new File("pcms/example0.pcm");
 		TraitementPcm trait = new TraitementPcm(file);
 
-		// generation avec un template html : freemarker
+		// Generation avec un template html : freemarker
 		GenerationHtml generator = new GenerationHtml(trait, "pcms/testHtml1.ftl" , "pcms/testHtml1.html");
+		// test charger nouveau pcm
+		generator.getTraitPcm().loadPcm(new File("pcms/example1.pcm"));
 		generator.generateHtml();
-
+		
 		// test sur feature/type dominant
 		/*Set<String> set1 = generator.getTraitPcm().getBestTypes().keySet();
 		Collection<String> set1 = generator.getTraitPcm().getBestTypes().values();
