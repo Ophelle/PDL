fb4ec1a17fc351e1dec84ce7dd1b9bb477039450
lewanni <quang532@gmail.com>
2016-11-07 21:43:36 +0100

    update du main avec les autre classes qui sont aussi update

--- src/main/java/org/opencompare/Main.java
+++ src/main/java/org/opencompare/Main.java
@@ -2,35 +2,27 @@ package org.opencompare;
 
 import java.io.File;
 import java.io.IOException;
-import java.util.ArrayList;
-import java.util.Collection;
-import java.util.HashMap;
-import java.util.Iterator;
-import java.util.List;
-import java.util.Map;
-import java.util.Set;
-import java.util.Map.Entry;
 
 import freemarker.template.TemplateException;
 
 public class Main {
 
 	public static void main(String[] args) throws IOException, TemplateException {
-		// chargement pcm
+		
+		// Chargement fichier + pcm + traitement des types
 		File file = new File("pcms/example.pcm");
 		TraitementPcm trait = new TraitementPcm(file);
-		trait.loadPcm();
-		//generation avec un template html : freemarker
-		GenerationHtml3 gen3 = new GenerationHtml3();
-		gen3.generateHtml3(trait.getFeatures(),gen3.getRealTypes(gen3.getTypes(trait)), "pcms/exampleV3.html", trait);
-		
-		//Set<String> set1 = gen3.getRealTypes(gen3.getTypes(trait)).keySet();
-		Collection<String> set1 = gen3.getRealTypes(gen3.getTypes(trait)).values();
-		
-		for(String str : set1) {
-			System.out.println(str);
-		}
-		
-	}
 
+		// generation avec un template html : freemarker
+		GenerationHtml generator = new GenerationHtml(trait, "pcms/testHtml1.ftl" , "pcms/testHtml1.html");
+		generator.generateHtml();
+
+		// test sur feature/type dominant
+		/*Set<String> set1 = generator.getTraitPcm().getBestTypes().keySet();
+		Collection<String> set1 = generator.getTraitPcm().getBestTypes().values();
+
+		for (String str : set1) {
+			System.out.println(str);
+		}*/
+	}
 }
