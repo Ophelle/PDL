179b4cc2b37bbe41137120ac211209fe2f639831
MOUSSA LYDIA <lyly.moussa@gmail.com>
2016-11-13 15:25:54 +0100

    Test TraitementPcm getPcm is not null

---
+++ src/test/java/org/opencompare/TraitementPcmTest.java
@@ -0,0 +1,30 @@
+package org.opencompare;
+
+import org.junit.Assert;
+import org.junit.Test;
+import org.opencompare.api.java.PCMElement;
+import org.opencompare.api.java.PCMFactory;
+import org.opencompare.api.java.Value;
+import org.opencompare.api.java.util.PCMVisitor;
+import org.opencompare.api.java.value.BooleanValue;
+import org.opencompare.model.impl.BooleanValueImpl;
+
+import java.io.File;
+import java.io.IOException;
+
+/**
+ * @author Moussa Lydia
+ */
+public class TraitementPcmTest {
+
+    @Test
+    public void testWhenFileExistsGetPcmIsNotNull() throws IOException {
+        // si le fichier pcm exist
+        final File file = new File("pcms/Comparison_between_Argentine_provinces_and_countries_by_GDP_(PPP)_per_capita_0.pcm");
+        // et que je crée le TraitementPcm
+        final TraitementPcm traitementPcm = new TraitementPcm(file);
+        // alors getPcm ne doit pas être null
+        Assert.assertNotNull(traitementPcm.getPcm());
+        Assert.assertEquals("Comparison_between_Argentine_provinces_and_countries_by_GDP_(PPP)_per_capita", traitementPcm.getPcm().getName());
+    }
+}
